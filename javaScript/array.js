796747ef75d66b2ce51c2e8f09729dae4d4867bc
Salman Malik <ssmalik989@gmail.com>
2024-09-06 02:40:27 +0530

    array

---
+++ javaScript/array.js
@@ -0,0 +1,32 @@
+let arr = [1, 2, 3, 4];
+
+// arr.push(5, 6);
+// let del = arr.pop();
+// console.log(del);
+// let newArr = arr.concat(7, 8);
+// // console.log(newArr.indexOf(8));
+// console.log(newArr);
+// let join = newArr.join("");
+// let join1 = newArr.join("@");
+
+// console.log(typeof join);
+// console.log(join1);
+
+// // function baby() {
+// //   return "something";
+// // }
+
+// // console.log(baby.name);
+// arr.reverse();
+
+// arr.shift();
+// arr.unshift(3, 4);
+
+let sliceArr = arr.slice(1, 3);
+console.log(arr);
+console.log(sliceArr);
+
+let spliceArr = arr.splice(1, 3);
+
+console.log(arr);
+console.log(spliceArr);
